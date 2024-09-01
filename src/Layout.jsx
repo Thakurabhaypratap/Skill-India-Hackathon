b05b5db1decae606a16ade5c80a3f022144609f9
Abhay <abhaypratapsingh7704866570@gmail.com>
2024-09-01 14:32:17 -0700

    sih2

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import Header from './Components/Header/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
+import './Layout.css'
 
-function Layout() {
-  return (
-   <>
-   <Header/>
-    <Outlet/>
-    <Footer/>
-    </> 
 
-  )
-}
-
-export default Layout
+  const Layout = () => {
+    return (
+      <div className="layout">
+        <Header /> {/* Add Header here */}
+        <div className="main-container">
+          <Sidebar /> {/* Add Sidebar here */}
+          <div className="content">
+            <Outlet /> {/* Renders the nested routes */}
+          </div>
+        </div>
+        <Footer /> {/* Add Footer here */}
+      </div>
+    );
+  };
+  
+  export default Layout;
